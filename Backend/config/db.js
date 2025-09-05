0743da9b3cefbe57e9deeba7f8c1e6feefc403ff
Shantanu-Tiwari <shantanutiwari240704@gmail.com>
2025-09-05 06:21:09 +0530

    first commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Successfully connected to MongoDB Atlas!');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);
